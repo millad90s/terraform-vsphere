00af315d898912e7963a38482528af211c223c88
Milad <millad90s@gmail.com>
2021-04-28 17:27:28 +0430

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+
+
+variable "pub_key" {
+    default = "file/service_terraform.pub"
+}
+
+variable "vm_name" {
+  default = "TestDataBase"
+}
+variable "vm_domain" {
+    default = "internal"
+
+}
+variable "os_password" {
+    default = "admin"
+}
